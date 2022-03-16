acb96036a151be635441037cd189ff062948e8e0
jAmWhisk <whisk3yjam@gmail.com>
2022-03-15 21:20:07 -0400

    fix syntax error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const htmlRoutes = require('./routes/html-route');
 const apiRoutes = require('./routes/api-route');
-const PORT = process.env.PORT || 3001;;
+const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
